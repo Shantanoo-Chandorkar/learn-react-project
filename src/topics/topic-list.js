04de28ad73efa644ad57d7bc3cff8c780f27bf7f
Shantanoo Chandorkar <chandorkarshantanoo@gmail.com>
2025-04-27 06:31:03 +0530

    Added useEffect hook material and Side Effect Related Topic material to the project

--- src/topics/topic-list.js
+++ src/topics/topic-list.js
@@ -1,19 +1,39 @@
-// Import State Details
-import StateInformation from "../RelatedTopics/State/information";
 // Import useState Hook Details
 import useStateInformation from "../Hooks/UseStateComponent/information";
 import UseStateComponentExample from "../Hooks/UseStateComponent/UseStateComponentExample";
+// Import useEffect Hook Details
+import useEffectInformation from "../Hooks/UseEffectComponent/information";
+import UseEffectComponentExample from "../Hooks/UseEffectComponent/UseEffectComponentExample";
+// Import State Details
+import StateInformation from "../RelatedTopics/State/information";
+// Import Side Effects Details
+import SideEffectsInformation from "../RelatedTopics/SideEffects/information";
 
 export const topics = [
   {
     id: 1,
-    title: "state",
-    info: StateInformation,
+    title: "useState",
+    info: useStateInformation,
+    example: UseStateComponentExample,
+    category: "hooks",
   },
   {
     id: 2,
-    title: "useState",
-    info: useStateInformation,
-    example: UseStateComponentExample
-  }
+    title: "useEffect",
+    info: useEffectInformation,
+    example: UseEffectComponentExample,
+    category: "hooks",
+  },
+  {
+    id: 3,
+    title: "State",
+    info: StateInformation,
+    category: "related-topics",
+  },
+  {
+    id: 4,
+    title: "Side Effects",
+    info: SideEffectsInformation,
+    category: "related-topics",
+  },
 ];
