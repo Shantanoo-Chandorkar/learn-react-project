aea3f12be39d68fa36ccae0a087043884474c02a
Shantanoo-Chandorkar <chandorkarshantanoo@gmail.com>
2026-04-19 19:22:34 +0530

    Converted the tables into a readable format using remark-gfm

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import mdx from '@mdx-js/rollup';
+import remarkGfm from 'remark-gfm';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     mdx({
-      remarkPlugins: [],
+      remarkPlugins: [remarkGfm],
       rehypePlugins: [],
       providerImportSource: '@mdx-js/react',
     }),
